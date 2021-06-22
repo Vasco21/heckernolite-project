c47622679fdcc1f665ba37ac637965459e7a2a6b
71 <glen.hlongwane@ooba.co.za>
2021-06-23 00:29:33 +0200

    Changes

--- app.js
+++ app.js
@@ -7,16 +7,53 @@
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
-navToggle.addEventListener("click", function () {
-//   console.log(links.classList);
-//   console.log(links.classList.contains("random"));
-//   console.log(links.classList.contains("links"));
-//   if (links.classList.contains("show-links")) {
-//     links.classList.remove("show-links");
-//   } else {
-//     links.classList.add("show-links");
-//   }
-  links.classList.toggle("show-links");
-});
+var reportsList = [];
 
+// var modifiedReportsList = [];
+
+var REPORTS_KEY = "REPORTS";
+
+var reportsTemplate = document.querySelector(".reportsTemplate").innerHTML;
+
+var templateCompiler = Handlebars.compile(reportsTemplate);
+
+var setLocalStorageObject = function(key, Object) {
+  window.localStorage.setItem(key, JSON.stringify(Object));
+}
+
+var getLocalStorageObject = function(key) {
+  let temp = window.localStorage.getItem(key);
+  if (temp == null) {
+      return null;
+  }
+  return JSON.parse(temp);
+}
+
+reportsList = getLocalStorageObject(REPORTS_KEY);
+
+for(let i = 0 ; i < reportsList.length; i++) {
+
+}
+
+if(reportsList) {
+  // for(let i = 0 ; i < reportsList.length; i++) {
+  //   reportsList[i].image = URL.createObjectURL(reportsList[i].image);
+  //   modifiedReportsList.push(reportsList)
+  // }
+  
+  document.querySelector("#templateDisplay").innerHTML = templateCompiler({report : reportsList})
+}
+
+URL.createObjectURL
+// navToggle.addEventListener("click", function () {
+// //   console.log(links.classList);
+// //   console.log(links.classList.contains("random"));
+// //   console.log(links.classList.contains("links"));
+// //   if (links.classList.contains("show-links")) {
+// //     links.classList.remove("show-links");
+// //   } else {
+// //     links.classList.add("show-links");
+// //   }
+//   links.classList.toggle("show-links");
+// });
 
